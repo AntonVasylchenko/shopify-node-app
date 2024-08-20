92ead5b02f9f7e0756a94f5e90baa4413210d3aa
Vasylchenko Anton <87824792+Smd16Bi@users.noreply.github.com>
2024-08-20 07:14:18 +0300

    added log

--- controllers/webhooksControlls.js
+++ controllers/webhooksControlls.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 async function getWebhooksShopify(req,res) {
     console.log("123");
+    console.log(req.body);
     
     res.status(StatusCodes.OK).json({ msg: "hello" });
 }
